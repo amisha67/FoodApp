868e9503c98cd0738f971898d1f252beadf42948
amisha67 <167330962+amisha67@users.noreply.github.com>
2024-05-20 18:59:01 +0530

    Delete src directory

--- src/components/About.js
+++
@@ -1,9 +0,0 @@
-const About = () =>{
-    return (
-        <div>
-            <h1>This is about me</h1>
-            <h2>who learned this from akshay saini</h2>
-        </div>
-    )
-};
-export default About;
